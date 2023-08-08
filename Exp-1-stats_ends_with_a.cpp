ec156137944e0fe5f4752e9010f028e681aa67a3
Raghu1426 <113841807+Raghu1426@users.noreply.github.com>
2023-08-08 14:07:51 +0530

    Add files via upload

---
+++ Exp-1-stats_ends_with_a.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int i,l,flag=0,n;
+	char st[100];
+	printf("\nEnter any String: ");
+	scanf("%s",&st);
+	l=strlen(st);
+	for(i=0;i<l;i++)
+	{
+		if(st[i]!='a'&& st[i]!='b')
+		{
+			flag=1;
+		}
+	}
+	if(flag==1)
+	{
+		printf("\nInvalid string");
+	}
+	else
+	{
+		printf("\nValid String");
+		if(st[0] == 'a' && st[l-1] == 'a')
+			printf("\nString is Accepted for the given condition");
+		else
+			printf("\nString is Not Accepted for the given condition");
+	}
+	return 0;
+}
